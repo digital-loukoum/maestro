05b4dd51b16dfe2a70a37f0a28f901acc4b3e5d8
lepzulnag <gindre.matthieu@gmail.com>
2023-10-14 19:06:10 +0200

    🧪 test right slots array, should go recursive instead

--- source-v3/parserV3.ts
+++ source-v3/parserV3.ts
@@ -1,58 +1,112 @@
-import { Node, NodeMatcher, Parsable, Token, TokenDeclaration } from "./types"
+import { Context, Grammar, Node, Parsable, Slot, Token } from "./types"
 
-export function parserV3<Input extends Parsable>(tokens: Array<TokenDeclaration<Input>>) {
-	let offset = 0
+export function parserV3<Input extends Parsable>(grammar: Grammar<Input>) {
+	return (input: Input): Node<unknown> | undefined => {
+		const nodes: Array<Node<unknown>> = [] // should be a very small array, containing 2 elements max 99% of the time
+		const rightestSlots: Array<Slot> = [] // this array can grow quite much
+		// let token: Token | undefined = undefined
+		let node: Node<unknown> | undefined = undefined
+		// let slot: Node<unknown> | undefined = undefined
+		// const scopes: Array<Scope> = []
+		// let currentScope: Scope | undefined = undefined
+		// let noAvailableSlot = true
 
-	return (input: Input): Token | null => {
-		let result: Token | null = null
-		let nodes: Array<NodeMatcher>
-		let token: Token | null = null
-		let node: Node<unknown> | null = null
-		let tokenDeclaration!: TokenDeclaration<Input>
+		const context: Context<Input> = {
+			input,
+			start: 0,
+			useLeftNode,
+			useRightNode,
+		}
 
 		skipWhitespaces()
 
-		while (offset < input.length) {
-			// 1. we find the next token
-			for (tokenDeclaration of tokens) {
-				token = tokenDeclaration.match(input, offset)
-				if (token) break
-			}
-			if (!token) {
-				throw new Error(`Unexpected character at position ${offset}`)
-			}
-			offset = token.end
+		while (context.start < input.length) {
+			// console.log("scopes", scopes)
+			// noAvailableSlot = currentScope == undefined
 
-			// 2. we find the node associated with the token
-			// Inline this loop in precompiled mode (with the form `node = nodeMatcher1() ?? nodeMatcher2() ?? ...`)
-			for (const nodeMatcher of tokenDeclaration.nodes) {
-				node = nodeMatcher()
+			// 1. we find the next token
+			for (const rule of grammar) {
+				node = rule(context)
 				if (node) break
 			}
 			if (!node) {
-				throw new Error(`Unexpected token ${token.name} at position ${token.start}`)
+				throw new Error(`Unexpected character at position ${context.start}`)
 			}
 
+			context.start = node.end
+			const rightestSlot = rightestSlots[rightestSlots.length - 1]
+
 			// 3. we add the node to the AST
+			if (isSlotAvailable(rightestSlot)) {
+				console.log("Slot available", nodes, rightestSlots)
+				Object.assign(rightestSlot.node, node)
+			} else {
+				nodes.push(node)
+				rightestSlots.push({ node, priority: 0 })
+			}
+
+			skipWhitespaces()
 		}
 
-		return result
+		return nodes[0]
 
 		// -- UTILITY FUNCTIONS -- //
 		function skipWhitespaces() {
-			while (input[offset] == " ") {
-				offset++
+			while (input[context.start] == " ") {
+				context.start++
 			}
 		}
 
-		function getToken(): Token | null {
-			skipWhitespaces()
-			let result: Token | null = null
-			for (const token of tokens) {
-				result = token.match(input, offset)
-				if (result) break
+		function useLeftNode(
+			priority: number
+		): Node<unknown> | undefined {
+			if (rightestSlots.length == 0) {
+				throw new Error("No left node to use")
 			}
-			return result
+
+			for (let index = rightestSlots.length - 1; index >= 0; index--) {
+				const rightestSlot = rightestSlots[index]
+				if (isSlotAvailable(rightestSlot)) {
+					throw new Error("TODO: isSlotAvailable(rightestSlot)")
+				}
+				if (priority > rightestSlot.priority) {
+					// we found our left node
+					const leftNode = { ...rightestSlot.node as Node<unknown> };
+					(rightestSlot.node as any).name = undefined; // we make the slot available
+					return leftNode
+				}
+			}
+
+			console.log("Using left node. Rightest slot:", rightestSlot)
+
+			if (isSlotAvailable(rightestSlot)) {
+				throw new Error("TODO: isSlotAvailable(rightestSlot)")
+			} else if (rightestSlot.priority < priority) {
+				// replace the rightmost slot
+				rightestSlots.push({ node, priority })
+				return nodes.pop()
+			} else {
+				const leftNode = { ...(rightestSlot.node as Node<unknown>) }
+				rightestSlot.node = node
+				rightestSlot.priority = priority
+				return leftNode
+			}
+		}
+
+		function useRightNode(priority: number): Node<unknown> | undefined {
+			const node = {}
+			rightestSlots.push({ node, priority })
+			return node as Node<unknown>
+		}
+
+		function isSlotAvailable(slot: Slot | undefined) {
+			return slot && (slot.node as Node<unknown>).name === undefined
+		}
+
+		function emptySlot(slot: Slot) {
+			for (const key in slot.node) {
+				delete slot.node[key]
+			}
 		}
 	}
 }
--- source-v3/types.ts
+++ source-v3/types.ts
@@ -1,16 +1,28 @@
-export type TokenDeclaration<Input extends Parsable> = {
-	match: (input: Input, offset: number) => Token | null
-	nodes: Array<NodeMatcher>
-}
+export type Token = Node<{}> // a token object is a raw node without additional values
 
-export type Token = Node<undefined> // a token object is a raw node without additional values
-
-export type Node<Values> = {
+export type Node<Value> = {
 	name: string
 	start: number
 	end: number
-	raw: string
-} & (Values extends undefined ? {} : { values: Values })
+	// raw: string
+} & Value
 
-export type NodeMatcher = () => Node<unknown> | null
 export type Parsable = ArrayLike<unknown>
+
+export type Grammar<Input extends Parsable> = Array<Rule<Input>>
+
+export type Rule<Input extends Parsable> = (
+	context: Context<Input>
+) => undefined | Node<unknown>
+
+export type Context<Input extends Parsable> = {
+	input: Input
+	start: number
+	useLeftNode(node: Node<unknown>, priority: number): Node<unknown> | undefined
+	useRightNode(priority: number): Node<unknown> | undefined
+}
+
+export type Slot = {
+	node: Node<unknown> | {}
+	priority: number
+}
